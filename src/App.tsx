4d6ea15fcc60d96bb2c0b47b0e974ca8a96450e8
ericp84 <ericpaoli65@gmail.com>
2022-09-19 16:24:54 +0200

    fc

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+interface Person {
+  id: number;
+  email: string;
+  avatar: string;
+}
+
+function App() {
+  const [persons, setPersons] = useState<Person[]>([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const data = (await axios.get<{data: Person[]}>("https://reqres.in/api/users/")).data.data as Person[];
+      setPersons(data);
+    };
+    fetchImages();
+  }, []);
+  return (
+    <div className="App">
+      {persons.map((person) => (
+        <React.Fragment key={person.id}>
+          <p>{person.email}</p>
+          <img src={person.avatar} />
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+export default App;
